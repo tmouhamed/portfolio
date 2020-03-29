15cdae5dd40180019f97ff5aff6c721d9f7b841f
tmouhamed <s.toqaa@gmail.com>
2020-03-29 00:25:37 -0400

    add some animation and css styling

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -9,14 +9,18 @@ class Hero extends Component {
         return (
             <section className="hero">
                 <PageShape />
-                <Typist className="hero__typist">
-                    <div class="hero__caption">
-                        <h1 className="hero__title">Hello,<br /> my name is <span>Toqa</span></h1>
-                        <p className="hero__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                        <a href="/" className="hero__button">A little about me</a>
-                    </div>
-                </Typist>
 
+                <div class="hero__caption">
+                    <Typist cursor={{ show: false }} className="hero__typist">
+                        <h1 className="hero__typist-title">Hello,<br /> my name is <span>Toqa</span></h1>
+                        <Typist.Backspace count={15} delay={200} />
+                        <h1 className="hero__typist-title">I am a Web Developer</h1>
+                    </Typist>
+                    <div className="hero__subSection">
+                        <p className="hero__subSection-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        <a href="/" className="hero__subSection-button">A little about me</a>
+                    </div>
+                </div>
             </section>
         );
     }
